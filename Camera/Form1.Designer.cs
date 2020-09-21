0ecf9ae536fe08463a6051225247ffbbdcce247f
Antonio-Gabriel <antoniocamposgabriel@gmail.com>
2020-09-21 15:50:22 +0100

    Trabalhando com camera apartir do C#

---
+++ Camera/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Camera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pncamera = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pncamera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(399, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tirar Foto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(399, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Desligar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(399, 162);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 32);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Ligar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pncamera
+            // 
+            this.pncamera.Location = new System.Drawing.Point(68, 92);
+            this.pncamera.Name = "pncamera";
+            this.pncamera.Size = new System.Drawing.Size(239, 216);
+            this.pncamera.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pncamera.TabIndex = 3;
+            this.pncamera.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(548, 403);
+            this.Controls.Add(this.pncamera);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pncamera)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pncamera;
+    }
+}
+
---
+++ Camera/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using DirectX.Capture;
+using System.Drawing.Imaging;
+
+namespace Camera
+{
+    public partial class Form1 : Form
+    {
+        public DirectX.Capture.Filter Camera;
+        public DirectX.Capture.Capture CaptureInfo;
+        public DirectX.Capture.Filters CamContainer;
+        Image capturaImagem;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void AtualizaImagem(PictureBox frame)
+        {
+            try
+            {
+                capturaImagem = frame.Image;
+                this.pncamera.Image = capturaImagem;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro " + ex.Message);
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CamContainer = new DirectX.Capture.Filters();
+            try
+            {
+                int no_of_cam = CamContainer.VideoInputDevices.Count;
+
+                for (int i = 0; i < no_of_cam; i++)
+                {
+                    try
+                    {
+                        // obtém o dispositivo de entrada do vídeo
+                        Camera = CamContainer.VideoInputDevices[i];
+
+                        // inicializa a Captura usando o dispositivo
+                        CaptureInfo = new DirectX.Capture.Capture(Camera, null);
+
+                        // Define a janela de visualização do vídeo
+                        CaptureInfo.PreviewWindow = this.pncamera;
+
+                        // Capturando o tratamento de evento
+                        CaptureInfo.FrameCaptureComplete += AtualizaImagem;
+
+                        // Captura o frame do dispositivo
+                        CaptureInfo.CaptureFrame();
+
+                        // Se o dispositivo foi encontrado e inicializado então sai sem checar o resto
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                try {
+                    CaptureInfo.CaptureFrame();
+                }
+                catch (Exception ex) {
+                    MessageBox.Show(this,ex.Message);
+                }
+            }
+            catch { }
+        }
+    }
+}
